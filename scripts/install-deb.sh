9db81f78e84d64d798eaaaafdeeae96668402f7a
khai96_ <hvksmr1996@gmail.com>
2018-11-12 00:35:40 +0700

    Remove comments

--- scripts/install-deb.sh
+++ scripts/install-deb.sh
@@ -7,14 +7,9 @@ apt-get update -y || exit $?
 
 echo 'Installing necessary debian packages...'
 apt-get install -y \
-  # Will be removed
+  python3-pip git python3 \
   libtinfo-dev python3-dev build-essential \
   libzmq3-dev libcairo2-dev libpango1.0-dev \
   libmagic-dev libblas-dev liblapack-dev \
   wget \
-
-  # Won't be removed
-  python3-pip git python3 \
-
-  # Exit on error
   || exit $?
