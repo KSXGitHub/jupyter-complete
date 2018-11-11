4bb35cdd15cfc9d512a255af4eb5d8a7499e4ff0
khai96_ <hvksmr1996@gmail.com>
2018-11-12 01:13:21 +0700

    Remove python3 from apt

--- scripts/install-deb.sh
+++ scripts/install-deb.sh
@@ -7,7 +7,7 @@ apt-get update -y || exit $?
 
 echo 'Installing necessary debian packages...'
 apt-get install -y \
-  python3-pip git python3 \
+  python3-pip git \
   libtinfo-dev python3-dev build-essential \
   libzmq3-dev libcairo2-dev libpango1.0-dev \
   libmagic-dev libblas-dev liblapack-dev \
