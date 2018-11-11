ac547015773cfe41779a9521e1ca035d6bbb319f
khai96_ <hvksmr1996@gmail.com>
2018-11-11 21:58:40 +0700

    Add --compress

--- ci/docker-build.bash
+++ ci/docker-build.bash
@@ -1,4 +1,7 @@
 #! /usr/bin/env
 
 echo "Building $DOCKER_IMAGE..."
-docker build . -t "$DOCKER_IMAGE" --cache-from hvksmr1996/jupyter-complete:latest
+docker build . \
+  -t "$DOCKER_IMAGE" \
+  --cache-from hvksmr1996/jupyter-complete:latest \
+  --compress
