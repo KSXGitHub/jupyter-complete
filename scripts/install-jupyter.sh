ee11bf8ddce2cd8a5c44e9ac8d09dbc6ff60d621
khai96_ <hvksmr1996@gmail.com>
2018-11-11 23:08:28 +0700

    Add themes

--- scripts/install-jupyter.sh
+++ scripts/install-jupyter.sh
@@ -9,5 +9,9 @@ python3 -m pip install --upgrade virtualenv || exit $?
 echo 'Installing Jupyter Notebook...'
 python3 -m pip install jupyter || exit $?
 
+echo 'Installing themes...'
+python3 -m pip install --upgrade jupyterthemes || exit $?
+jt -t monokai || exit $?
+
 # Workaround: (Kernel Crashing) https://github.com/jupyter/notebook/issues/4050
 python3 -m pip install -U jupyter_console || exit $?
