6add8050ab5d5dc7f8072c8e3398d941a92606a5
khai96_ <hvksmr1996@gmail.com>
2018-11-12 03:30:15 +0700

    Also install npm

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -4,7 +4,7 @@ echo 'NODEJS'
 
 echo 'Installing Node.js...'
 wget -O - https://deb.nodesource.com/setup_11.x | bash || exit $?
-apt-get install -y nodejs || exit $?
+apt-get install -y nodejs npm || exit $?
 
 echo 'Installing IJavaScript...'
 npm install --global ijavascript || exit $?
