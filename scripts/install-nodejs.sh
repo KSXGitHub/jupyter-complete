f3aa3e41eb0a28ccc92edd3d3063f05a51d851ec
khai96_ <hvksmr1996@gmail.com>
2018-11-12 02:36:51 +0700

    Install jupter-nodejs

---
+++ scripts/install-nodejs.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+
+echo 'NODEJS'
+
+echo 'Installing Node.js...'
+wget -O - https://deb.nodesource.com/setup_11.x | bash || exit $?
+apt-get install -y nodejs || exit $?
+
+echo 'Installing jupyter-nodejs...'
+git clone https://github.com/notablemind/jupyter-nodejs.git || exit $?
+cd jupyter-nodejs
+mkdir -p ~/.ipython/kernels/nodejs/ || exit $?
+npm install && node install.js || exit $?
+npm run build || exit $?
+npm run build-ext || exit $?
