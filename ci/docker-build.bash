14e050cbd42667923ead4c2d3dd4eec110de2277
khai96_ <hvksmr1996@gmail.com>
2018-11-11 22:05:42 +0700

    master is latest

--- ci/docker-build.bash
+++ ci/docker-build.bash
@@ -4,4 +4,16 @@ echo "Building $DOCKER_IMAGE..."
 docker build . \
   -t "$DOCKER_IMAGE" \
   --cache-from hvksmr1996/jupyter-complete:latest \
-  --compress
+  --compress || exit $?
+
+if [[ "$GIT_TAG" == 'master' ]]; then
+  echo 'Branch is "master"'
+  echo 'Tagging as "latest"'
+
+  if [[ -z "$DOCKER_LATEST" ]]; then
+    echo 'Missing $DOCKER_LATEST'
+    exit -1
+  fi
+
+  docker tag "$DOCKER_IMAGE" "$DOCKER_LATEST"
+fi
