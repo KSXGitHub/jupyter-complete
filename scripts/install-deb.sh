0d38d1d4ce2e7079c66079a144ed042ee194178f
khai96_ <hvksmr1996@gmail.com>
2018-11-12 02:31:29 +0700

    Add themes

--- scripts/install-deb.sh
+++ scripts/install-deb.sh
@@ -10,4 +10,5 @@ apt-get install -y \
   python3-pip git libtinfo-dev \
   python3-dev build-essential \
   libzmq3-dev libcairo2-dev libpango1.0-dev \
-  libmagic-dev libblas-dev liblapack-dev || exit $?
+  libmagic-dev libblas-dev liblapack-dev \
+  || exit $?
--- scripts/install-jupyter.sh
+++ scripts/install-jupyter.sh
@@ -11,3 +11,7 @@ python3 -m pip install jupyter || exit $?
 
 # Workaround: (Kernel Crashing) https://github.com/jupyter/notebook/issues/4050
 python3 -m pip install -U jupyter_console || exit $?
+
+echo 'Installing themes...'
+python3 -m pip install --upgrade jupyterthemes || exit $?
+jt -t monokai || exit $?
