dd2985a557424aaa9e31641ed948dd706f6ebfa4
khai96_ <hvksmr1996@gmail.com>
2018-11-12 18:11:21 +0700

    Rearrange install steps

--- scripts/install-haskell.sh
+++ scripts/install-haskell.sh
@@ -2,11 +2,11 @@
 
 echo 'HASKELL'
 
-echo 'Updating Cabal...'
-cabal install Cabal cabal-install || exit $?
-
 echo 'Updating package list...'
 cabal update || exit $?
+
+echo 'Updating Cabal...'
+cabal install Cabal cabal-install || exit $?
 
 echo 'Installing IHaskell...'
 cabal install ihaskell || exit $?
